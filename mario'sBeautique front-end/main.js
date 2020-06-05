1ddc8db1b97ba77077e025332a7020925ae468eb
IlariaAttisani <66007907+IlariaAttisani@users.noreply.github.com>
2020-06-05 19:42:16 +0200

    Add files via upload
    changes galleria gv

---
+++ mario'sBeautique front-end/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){ 
+
+      $(".menu").hover(function() {        
+          $(this).toggleClass("change"); 
+          
+      });
+     
+ 
+ var slideIndex = 1;
+ showSlides(slideIndex);
+ // Next/previous controls
+ $(".frecce").on('click',function () {
+   let n = +$(this).attr("freccia")
+   showSlides(slideIndex += n)
+  })
+  
+  // Thumbnail image controls
+  $(".demo").on('click' ,function () {
+    let n=$(this).attr("slideNumber")
+    slideIndex = n;
+    showSlides(slideIndex);
+  })
+
+
+      function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        var dots = document.getElementsByClassName("demo");
+        var captionText = document.getElementById("caption");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";
+        dots[slideIndex-1].className += " active";
+        captionText.innerHTML = dots[slideIndex-1].alt;
+      }  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
